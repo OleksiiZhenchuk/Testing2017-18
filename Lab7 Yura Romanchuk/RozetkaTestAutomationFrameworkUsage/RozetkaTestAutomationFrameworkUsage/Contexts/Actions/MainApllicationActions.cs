a102dd501ca1d360d96940bd9fe1bf0ebe237b47
RaannddooM <33698979+RaannddooM@users.noreply.github.com>
2017-12-18 21:34:06 +0200

    Add files via upload

---
+++ Lab7 Yura Romanchuk/RozetkaTestAutomationFrameworkUsage/RozetkaTestAutomationFrameworkUsage/Contexts/Actions/MainApllicationActions.cs
@@ -0,0 +1,59 @@
+﻿using RozetkaTestAutomationFrameworkUsage.Pages;
+using System.Text.RegularExpressions;
+using RozetkaTestAutomationFrameworkUsage.Utils;
+using OpenQA.Selenium;
+
+namespace RozetkaTestAutomationFrameworkUsage.Contexts.Actions
+{
+    public static class MainApllicationActions
+    {
+        public static WineListPage ClickOnMore(this WineListPage page, Waiters wait)
+        {
+            wait.waitForClickableElement(By.Name(page.locLinkMore));
+            page.LinkMore.Click();
+            return page;
+        }
+
+        public static WineListPage ChooseCountry(this WineListPage page, string country, Waiters wait)
+        {
+            foreach (var element in page.listOfCountry)
+            {
+                if (Regex.IsMatch(element.getText(), country))
+                {
+                    element.Click();
+                    break;
+                }
+            }
+            return page;
+        }
+
+        public static GoodsItemPage ClickOnButtonBuy(GoodsItemPage page, Waiters wait)
+        {
+            wait.waitForVisibilityElement(By.ClassName(page.locBuy));
+            page.Buy.Click();
+            return page;
+        }
+
+        public static GoodsItemPage SubmitOfferButton(this GoodsItemPage page, Waiters wait)
+        {
+            wait.waitForVisibilityElement(By.Id(page.locSubOffer));
+            page.SubmitOffer.Click();
+            return page;
+        }
+
+        public static CheckOutPage ClickOnButtonContinue(CheckOutPage page)
+        {
+            while(page.Continue.Displayed && page.Continue.Enabled)
+            {
+                page.Continue.Click();
+            }
+            return page;
+        }
+
+        public static CheckOutPage ClickOnNotCall(this CheckOutPage page, Waiters wait)
+        {
+            page.NotCall.Click();     
+            return page;
+        }
+    }
+}
---
+++ Lab7 Yura Romanchuk/RozetkaTestAutomationFrameworkUsage/RozetkaTestAutomationFrameworkUsage/Utils/Waiters.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Support.UI;
+using RozetkaTestAutomationFrameworkUsage.Pages;
+using OpenQA.Selenium;
+using System;
+
+namespace RozetkaTestAutomationFrameworkUsage.Utils
+{
+    public class Waiters : BasePage
+    {
+        public Waiters(IWebDriver driver) : base(driver)
+        { }
+
+        public void waitForClickableElement(By bylocator)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(bylocator));
+        }
+
+        public void waitForVisibilityElement(By bylocator)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(bylocator));
+        }
+    }
+}
